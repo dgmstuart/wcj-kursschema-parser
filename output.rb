ad48b67d995f60da27a8d9dc36433f622ca6673b
Duncan Stuart <dgmstuart@gmail.com>
2025-02-03 01:49:34 +0100

    Tweaks to output
    Just messing about really

--- output.rb
+++ output.rb
@@ -1,12 +1,24 @@
+#!/usr/bin/env ruby
+
 require "./app/schema_parser"
 
 file_path = "spec/fixtures/Schema kurser HT24-VT25 - Stora salen 24_25.csv"
 
 courses = SchemaParser.new.parse(file_path)
 
-courses.each do |course_id, course|
-  weeks = course.weeknight_weeks.join(", ")
-  weekends = course.weekend_weeks.join(", ")
+def week_string(weeks)
+  "weeks #{weeks.join(", ")}"
+end
 
-  puts "#{course_id} - weeks #{weeks} - weekends #{weekends}"
+def weekend_string(weekends)
+  return "(no weekend)" if weekends.empty?
+
+  "weekend #{weekends.join(", ")}"
+end
+
+courses.each do |course_id, course|
+  weeks = course.weeknight_weeks
+  weekends = course.weekend_weeks
+
+  puts "#{course_id.ljust(10)} - #{week_string(weeks).ljust(26)} - #{weekend_string(weekends)}"
 end
