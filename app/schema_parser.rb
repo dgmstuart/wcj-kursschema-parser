e93e0dd8a09c3fb128431ec913cd188b1f9f4800
Duncan Stuart <dgmstuart@gmail.com>
2025-02-04 16:26:08 +0100

    Refactor: Extract some Term classes
    Some of this is maybe just shuffling code around.

--- app/schema_parser.rb
+++ app/schema_parser.rb
@@ -7,82 +7,135 @@ class SchemaParser
   WEEK_HEADER = "Vecka"
   COURSES_HEADER = "Kurser       Lindy /Jazz"
 
-  TERM_ID_REGEX = /(HT|VT)\d+/
+  NON_COURSE_STRINGS = ["Önskekurser", "Öppet hus", "Registreringen stänger", "Första kursdag"]
 
   WEEKDAY_COLUMN_NUMBERS = (1...16)
   WEEKEND_COLUMN_NUMBERS = (16...22)
 
   def parse(file_path)
+    data = CSV.read(file_path, headers: true)
 
-    data = CSV.foreach(file_path, headers: true)
-    week_rows = data.reject { |row| row[WEEK_HEADER].nil? }
+    _autumn_term, spring_term = TermsBuilder.new.build(data)
 
-    spring_year = terms(data).last.year
-    spring_term_rows = week_rows.reject { |row| Integer(row[WEEK_HEADER]) > 25 }
-
-    # NOTE: not always accurate to assume overlap with Spring term rows - sometimes course list overflows this.
-    spring_course_ids = course_ids(spring_term_rows)
-
-    courses(course_ids: spring_course_ids, period_data: spring_term_rows, year: spring_year)
+    term_courses(data:, term: spring_term)
   end
 
   private
 
-  Term = Data.define(:id, :row_index) do
+  def term_courses(data:, term:)
+    term_rows = data[term.row_numbers]
+    term_schedule_rows = term_rows.reject { |row| row[WEEK_HEADER].nil? }
+    course_ids = course_ids(term_rows)
+
+    courses(course_ids:, schedule_data: term_schedule_rows, year: term.year)
+  end
+
+  class AutumnTerm
+    def initialize(autumn_term:, spring_term1:)
+      @autumn_term = autumn_term
+      @spring_term1 = spring_term1
+    end
+
+    def year
+      @autumn_term.year
+    end
+
+    def row_numbers
+      first = @autumn_term.row_index + 1
+      last = @spring_term1.row_index - 1
+      (first..last)
+    end
+  end
+
+  class SpringTerm
+    def initialize(spring_term1:)
+      @spring_term1 = spring_term1
+    end
+
+    def year
+      @spring_term1.year
+    end
+
+    def row_numbers
+      first = @spring_term1.row_index + 1
+      (first..)
+    end
+  end
+
+  class TermsBuilder
+    TERM_ID_REGEX = /(HT|VT)\d+/
+
+    def build(data)
+      autumn_term, spring_term1, _spring_term2 = build_info(data)
+      [
+        AutumnTerm.new(autumn_term:, spring_term1:),
+        SpringTerm.new(spring_term1:)
+      ]
+    end
+
+    private
+
+    def build_info(data)
+      header_rows(data).map do |row, row_index|
+        text = row.fields.find do |field|
+          header_field?(field:)
+        end
+
+        TermInfo.new(id: text[TERM_ID_REGEX], row_index:)
+      end
+    end
+
+    def header_rows(data)
+      data.each_with_index.select do |row, index|
+        row.fields.any? do |field|
+          header_field?(field:)
+        end
+      end
+    end
+
+    def header_field?(field:)
+      field && field.match?(/Stora.+#{TERM_ID_REGEX}/)
+    end
+  end
+
+  TermInfo = Data.define(:id, :row_index) do
     def year
       Integer("20" + id[/\d+/])
     end
   end
 
-  def terms(data)
-    header_rows(data).map do |row, row_index|
-      text = row.fields.find do |field|
-        header_field?(field:)
-      end
-
-      Term.new(id: text[TERM_ID_REGEX], row_index:)
-    end
-  end
-
-  def header_rows(data)
-    data.each_with_index.select do |row, index|
-      row.fields.any? do |field|
-        header_field?(field:)
-      end
-    end
-  end
-
-  def header_field?(field:)
-    field && field.match?(/Stora.+#{TERM_ID_REGEX}/)
-  end
-
   def course_ids(data)
     data
-      .reject { |row| row[COURSES_HEADER].nil? }
-      .reject { |row| row[COURSES_HEADER].include?("Önskekurser") }
+      .reject { |row| not_a_course?(row[COURSES_HEADER]) }
       .map { |row| row[COURSES_HEADER] }
       .map { |course_id| remove_all_after_first_digit(course_id) }
+  end
+
+  def not_a_course?(value)
+    return true if value.nil?
+
+    NON_COURSE_STRINGS.any?{ |string| value.include?(string) }
   end
 
   def remove_all_after_first_digit(string)
     string.gsub(/^([^0-9]*[0-9]).*/, '\1')
   end
 
-  def courses(course_ids:, period_data:, year:)
+  def courses(course_ids:, schedule_data:, year:)
     course_ids.each_with_object({}) do |course_id, hash|
-      hash[course_id] = Course.new(period_data:, course_id:, year:)
+      hash[course_id] = Course.new(schedule_data:, course_id:, year:)
     end
   end
 
   class Course
     def initialize(
       course_id:,
-      period_data:,
+      schedule_data:,
       year:,
       date_parser: SchemaParser::DateParser.new
     )
       @course_id = course_id
-      @period_data = period_data
+      @schedule_data = schedule_data
       @year = year
       @date_parser = date_parser
     end
@@ -106,7 +159,7 @@ class SchemaParser
     private
 
     def dates(column_numbers:)
-      @period_data.map do |row|
+      @schedule_data.map do |row|
         fields = row.fields[column_numbers]
         dates_from_fields(fields)
       end.flatten
